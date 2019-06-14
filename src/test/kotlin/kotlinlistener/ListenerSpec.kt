86789909fb4228b1b509a8bcf1881d35374e3db5
Jeff Scott Brown <brownj@objectcomputing.com>
2019-06-14 13:45:34 -0500

    fix test

--- src/test/kotlin/kotlinlistener/ListenerSpec.kt
+++ src/test/kotlin/kotlinlistener/ListenerSpec.kt
@@ -3,11 +3,13 @@ package kotlinlistener
 import io.kotlintest.matchers.boolean.shouldBeTrue
 import io.kotlintest.specs.StringSpec
 import io.micronaut.context.ApplicationContext
+import io.micronaut.runtime.server.EmbeddedServer
 
 class ListenerSpec : StringSpec({
 
     "test listener"() {
-        val ctx = ApplicationContext.run()
+        val server = ApplicationContext.run(EmbeddedServer::class.java)
+        val ctx = server.applicationContext
         val bean = ctx.getBean(MyListener::class.java)
         bean.hasRun.shouldBeTrue()
     }
